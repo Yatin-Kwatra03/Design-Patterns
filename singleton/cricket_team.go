a7d2dbb36cc261ee34eab4e97ed15b09a09de276
Yatin Kwatra <yatinkwatra@Yatins-MacBook-Pro.local>
2024-03-05 09:14:50 +0530

    fix implementation for singleton pattern

--- singleton/cricket_team.go
+++ singleton/cricket_team.go
@@ -1,11 +1,14 @@
 package singleton
 
+import "sync"
+
 type CricketTeam struct {
 	Name string
 }
 
 var (
 	cricketTeamInstance *CricketTeam
+	once                sync.Once
 	InstanceNo          int
 )
 
@@ -14,11 +17,12 @@ var (
 // we don't have a constructor concept, anyone can just instantiate &CricketTeam{}. But it we follow the logic
 // to always go to the domain service to get the instance, then it can be implemented in this way.
 func newCricketTeam() *CricketTeam {
-	if cricketTeamInstance == nil {
+	once.Do(func() {
 		cricketTeamInstance = &CricketTeam{
 			Name: "I am instantiated",
 		}
 		InstanceNo++
-	}
+	},
+	)
 	return cricketTeamInstance
 }
