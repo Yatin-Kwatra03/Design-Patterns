1da0e543d3095d53a2a557262c707e1f96acb515
Yatin Kwatra <Yatinkwatra@gmail.com>
2025-06-12 14:02:09 +0530

    Create factory_pattern_in_c++.cpp

---
+++ factory_pattern_in_c++.cpp
@@ -0,0 +1,58 @@
+#include<iostream>
+using namespace std;
+
+
+enum class ShapeType {
+	DEFAULT,
+	CIRCLE,
+	SQUARE
+};
+
+class figureFeatures {
+public:
+	virtual void showShape() = 0;
+};
+
+class Circle: public figureFeatures {
+public:
+	void showShape() override {
+		cout << "Got a Circle\n";
+	}
+};
+
+
+class Square : public figureFeatures {
+public:
+	void showShape() override {
+		cout << "Got a Square\n";
+	}
+};
+
+figureFeatures* getFactoryImplementation(ShapeType requiredShape) {
+	switch (requiredShape) {
+	case ShapeType::CIRCLE:
+		return new Circle();
+	case ShapeType::SQUARE:
+		return new Square();
+	default:
+		return NULL;
+	}
+}
+
+int main() {
+	string s;
+	cin >> s;
+
+	ShapeType requiredShape = ShapeType::DEFAULT;
+
+	if (s == "Circle") {
+		requiredShape = ShapeType::CIRCLE;
+	}
+	else if (s == "Square") {
+		requiredShape = ShapeType::SQUARE;
+	}
+
+	auto relevantImplementation = getFactoryImplementation(requiredShape);
+	if (relevantImplementation != NULL) relevantImplementation->showShape();
+	else cout << "DAMN\n";
+}
